ac101ea460f8fcacdbedb223d10747a47b7e850f
0x26res <arthur.andres@gmail.com>
2021-05-06 21:29:35 +0100

    Clean up

--- tests/converter_test.py
+++ tests/converter_test.py
@@ -2,12 +2,18 @@ import unittest
 
 import pyarrow
 
-from arrowgen.converter import messages_to_table
+from arrowgen.converter import messages_to_table, get_arrow_schema
 from tests.data_generator import generate_messages
 from tests.generator_test import get_all_descriptors
 
 
-class ConverterTest(unittest.TestCase):
+class SchemaConverterTest(unittest.TestCase):
+    def test_get_arrow_schema(self):
+        for descriptor in get_all_descriptors():
+            self.assertIsInstance(get_arrow_schema(descriptor), pyarrow.Schema)
+
+
+class MessageConverterTest(unittest.TestCase):
     def test_all_messages(self):
         for message_descriptor in get_all_descriptors():
             messages = generate_messages(message_descriptor, 20)
@@ -15,6 +21,8 @@ class ConverterTest(unittest.TestCase):
             self.assertIsInstance(table, pyarrow.Table)
             self.assertEqual(len(table), 20)
 
+
+class LearningTest(unittest.TestCase):
     def test_offset_behavior(self):
         array = pyarrow.ListArray.from_arrays([0, 1, 6], [0, 1, 2, 3, 4, 5, 6])
         self.assertEqual(len(array), 2)
